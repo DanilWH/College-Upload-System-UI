db3f3595ea3d1b0ee1f1eebc90ae1a4c34db0c29
DanilWH <danil-lomakin-02@mail.ru>
2022-02-08 17:19:30 +0300

    [IMPLEMENTED] two services for: groups resource and user resource.

---
+++ src/app/_services/group.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Group } from "../_domains/group";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+
+const API_URL = environment.apiBaseUrl;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GroupService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    public list(): Observable<Group[]> {
+        return this.http.get<Group[]>(`${API_URL}/groups`);
+    }
+
+    public create(group: Group): Observable<Group> {
+        return this.http.post<Group>(`${API_URL}/groups`, group);
+    }
+
+    public update(group: Group): Observable<Group> {
+        return this.http.put<Group>(`${API_URL}/groups/${group.id}`, group);
+    }
+
+    public delete(groupId: number): Observable<void> {
+        return this.http.delete<void>(`${API_URL}/groups/${groupId}`);
+    }
+}
---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { User } from "../_domains/user";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+
+const API_URL = environment.apiBaseUrl;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    public getOne(userId: number): Observable<User> {
+        return this.http.get<User>(`${API_URL}/users/${userId}`);
+    }
+
+    public list(groupId: number): Observable<User[]> {
+        return this.http.get<User[]>(`${API_URL}/groups/${groupId}/users`);
+    }
+
+    public generateForGroup(groupId: number, formData: FormData): Observable<User[]> {
+        return this.http.post<User[]>(`${API_URL}/groups/${groupId}/users`, formData);
+    }
+
+    public deactivateOne(userId: number): Observable<void> {
+        return this.http.patch<void>(`${API_URL}/users/${userId}/status`, {});
+    }
+
+    public deactivateByGroup(groupId: number): Observable<void> {
+        return this.http.patch<void>(`${API_URL}/groups/${groupId}/users/status`, {});
+    }
+
+
+
+}
